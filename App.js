5d77f70c14d9d2e768d3b51bc95e5c836190b2dc
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-06-15 20:34:49 +0300

    Do image picker

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { FileSystemUploadType } from "expo-file-system";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [image, setImage] = useState(
+    "https://cdn.sick.com/media/ZOOM/2/82/782/IM0077782.png"
+  );
+  const [text, setText] = useState("Pick an image");
+
   const handleOcr = async () => {
-    console.log('handleOcr');
+    console.log("handleOcr");
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      // FileSystem.uploadAsync()
+    }
   };
 
   return (
@@ -12,7 +34,7 @@ export default function App() {
         <Image
           style={styles.card_image}
           source={{
-            uri: 'https://cdn.sick.com/media/ZOOM/2/82/782/IM0077782.png',
+            uri: image,
           }}
         />
         <View style={styles.text_container}>
@@ -28,13 +50,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   card_template: {
     width: 250,
     height: 250,
-    boxShadow: '10px 10px 17px -12px rgba(0,0,0,0.75)',
+    boxShadow: "10px 10px 17px -12px rgba(0,0,0,0.75)",
   },
   card_image: {
     width: 250,
@@ -42,17 +64,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   text_container: {
-    position: 'absolute',
+    position: "absolute",
     width: 250,
     height: 30,
     bottom: 0,
     padding: 5,
-    backgroundColor: 'rgba(0,0,0, 0.3)',
+    backgroundColor: "rgba(0,0,0, 0.3)",
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
   },
   card_title: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
   },
 });
